a8a9bd47b8a0ee86cad408a70e3a0697d11cab15
furebo <furebodidace582@gmail.com>
2022-04-20 16:01:24 +0200

    readme file added

---
+++ README.md
@@ -0,0 +1,1 @@
+![CI]('https://github.com/furebo/githubactionsexample2/actions/workflows/actions.yml/badge.svg')
--- test/app.test.js
+++ test/app.test.js
@@ -6,16 +6,15 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe('My welcome message test',()=>{
-    before((done)=>{
-        done();
-    })
-    it('should respond with a welcome message',()=>{
+  
+    it('should respond with a welcome message',(done)=>{
         chai.request(app)
         .get('/')
         .end((err,res)=>{
             res.body.should.have.property('message').eql('Welcome to my application');
             
         })
+        done()
     })
 })
 
