734bfc3abe056e5beffae599666c2ee276c81109
furebo <furebodidace582@gmail.com>
2022-04-20 10:59:25 +0200

    installed the babel dependances

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,27 @@
-import express from 'express';
+'use strict';
 
-const app = express();
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 
-app.get('/', (req, res) => {
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.get('/', function (req, res) {
     res.send({ message: 'Welcome to my application' });
 });
 
-app.get('/api', (req, res) => {
+app.get('/api', function (req, res) {
     res.send({ message: 'please enter your credentials' });
 });
 
-app.listen(3000, () => {
+app.listen(3000, function () {
     console.log("App is listening on port 3000!!!");
 });
 
-export default app;
+exports.default = app;
