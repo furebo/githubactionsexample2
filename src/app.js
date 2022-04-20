008f4c82084b40b4d64943e9601ac1aea7bc51e5
furebo <furebodidace582@gmail.com>
2022-04-20 10:17:45 +0200

    the new route added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send({message:'Welcome to my application'});
 })
 
+app.get('/api',(req,res)=>{
+    res.send({message:'please enter your credentials'});
+})
+
 app.listen(3000,()=>{
     console.log("App is listening on port 3000!!!");
 })
